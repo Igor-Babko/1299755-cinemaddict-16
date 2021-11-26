1f32d067cc0c864548fa6ecc51d5423523a5e9cc
Alisa <Workbabko@yandex.ru>
2021-11-26 23:23:08 +0300

    Добавил часть функций

---
+++ public/bundle.js
@@ -0,0 +1,18 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+const RenderPosition = {
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend'
+};
+
+const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
